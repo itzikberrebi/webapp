a809794371335f5a4a327198e1e4dbf5a24deac8
itzikberrebi <itzikb800@gmail.com>
2015-08-21 09:57:39 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -28,6 +28,25 @@ jQuery(document).ready(function() {
 		});
 	}
 
+	$.fn.loadquickActions = function(){ 
+		$.getJSON("data/config.json", function(data){
+			var quickActions=data.quickActions;
+			console.log('quickActions');
+			$(".action-list:first-child li:first-child" ).attr("href",quickActions[0].actions[0].url);
+			$(".action-list:first-child li:nth-child(2)" ).attr("href",quickActions[0].actions[1].url);
+			$(".action-list:first-child li:last-child" ).attr("href",quickActions[0].actions[2].url);
+
+			$(".action-list:nth-child(2) li:first-child" ).attr("href",quickActions[1].actions[0].url);
+			$(".action-list:nth-child(2) li:nth-child(2)" ).attr("href",quickActions[1].actions[1].url);
+			$(".action-list:nth-child(2) li:last-child" ).attr("href",quickActions[1].actions[2].url);
+
+			$(".action-list:last-child li:first-child" ).attr("href",quickActions[2].actions[0].url);
+			$(".action-list:last-child li:nth-child(2)" ).attr("href",quickActions[2].actions[1].url);
+			$(".action-list:last-child li:last-child" ).attr("href",quickActions[2].actions[2].url);
+
+		});
+	}
+
 	$.fn.loadJsonSecondery = function(){
 		console.log('load for second time')
 		$('input[name="site1name"]').val(tabs[0].options.sites[0].name);
