c74bd2ac42f175d063ccd651fd478936c3ef21f3
itzikberrebi <itzikb800@gmail.com>
2015-08-21 10:43:13 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -33,16 +33,25 @@ jQuery(document).ready(function() {
 			var quickActions=data.quickActions;
 			console.log(quickActions[0].actions[0].url);
 			$(".action-list:first-child li:first-child a" ).attr("href",quickActions[0].actions[0].url);
+			$(".action-list:first-child li:first-child a" ).html(quickActions[0].actions[0].label);
 			$(".action-list:first-child li:nth-child(2) a" ).attr("href",quickActions[0].actions[1].url);
+			$(".action-list:first-child li:nth-child(2) a" ).html(quickActions[0].actions[1].label);
 			$(".action-list:first-child li:last-child a" ).attr("href",quickActions[0].actions[2].url);
+			$(".action-list:first-child li:last-child a" ).html(quickActions[0].actions[2].label);
 
 			$(".action-list:nth-child(2) li:first-child a" ).attr("href",quickActions[1].actions[0].url);
 			$(".action-list:nth-child(2) li:nth-child(2) a" ).attr("href",quickActions[1].actions[1].url);
 			$(".action-list:nth-child(2) li:last-child a" ).attr("href",quickActions[1].actions[2].url);
+			$(".action-list:nth-child(2) li:first-child a" ).html(quickActions[0].actions[0].label);
+			$(".action-list:nth-child(2) li:nth-child(2) a" ).html(quickActions[0].actions[1].label);
+			$(".action-list:nth-child(2) li:last-child a" ).html(quickActions[0].actions[2].label);
 
 			$(".action-list:last-child li:first-child a").attr("href",quickActions[2].actions[0].url);
 			$(".action-list:last-child li:nth-child(2) a").attr("href",quickActions[2].actions[1].url);
 			$(".action-list:last-child li:last-child a").attr("href",quickActions[2].actions[2].url);
+			$(".action-list:last-child li:first-child a").html(quickActions[0].actions[0].label);
+			$(".action-list:last-child li:nth-child(2) a").html(quickActions[0].actions[1].label);
+			$(".action-list:last-child li:last-child a").html(quickActions[0].actions[2].label);
 
 		});
 	}
@@ -226,5 +235,5 @@ jQuery(document).ready(function() {
 	$.fn.loadJson();
 	$.fn.loadnotification();
 	$.fn.loadquickActions();
-	
+
 });
