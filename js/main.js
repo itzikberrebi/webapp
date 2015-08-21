db61de6b1b54ce193c5e88249830cb00999e631f
itzikberrebi <itzikb800@gmail.com>
2015-08-21 11:34:44 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -32,26 +32,26 @@ jQuery(document).ready(function() {
 		$.getJSON("data/config.json", function(data){
 			var quickActions=data.quickActions;
 			console.log(quickActions[1].actions[2].label);
-			$(".action-list:first-child li:first-child a" ).attr("href",quickActions[0].actions[0].url);
-			$(".action-list:first-child li:first-child a" ).html(quickActions[0].actions[0].label);
-			$(".action-list:first-child li:nth-child(2) a" ).attr("href",quickActions[0].actions[1].url);
-			$(".action-list:first-child li:nth-child(2) a" ).html(quickActions[0].actions[1].label);
-			$(".action-list:first-child li:last-child a" ).attr("href",quickActions[0].actions[2].url);
-			$(".action-list:first-child li:last-child a" ).html(quickActions[0].actions[2].label);
+			$("nav .action-list:first-child li:first-child a" ).attr("href",quickActions[0].actions[0].url);
+			$("nav .action-list:first-child li:first-child a" ).html(quickActions[0].actions[0].label);
+			$("nav .action-list:first-child li:nth-child(2) a" ).attr("href",quickActions[0].actions[1].url);
+			$("nav .action-list:first-child li:nth-child(2) a" ).html(quickActions[0].actions[1].label);
+			$("nav .action-list:first-child li:last-child a" ).attr("href",quickActions[0].actions[2].url);
+			$("nav .action-list:first-child li:last-child a" ).html(quickActions[0].actions[2].label);
 
-			$(".action-list:nth-child(2) li:first-child a" ).attr("href",quickActions[1].actions[0].url);
-			$(".action-list:nth-child(2) li:nth-child(2) a" ).attr("href",quickActions[1].actions[1].url);
-			$(".action-list:nth-child(2) li:last-child a" ).attr("href",quickActions[1].actions[2].url);
-			$(".action-list:nth-child(2) li:first-child a" ).html(quickActions[1].actions[0].label);
-			$(".action-list:nth-child(2) li:nth-child(2) a" ).html(quickActions[1].actions[1].label);
-			$(".action-list:nth-child(2) li:last-child a" ).html(quickActions[1].actions[2].label);
+			$("nav .action-list:nth-child(2) li:first-child a" ).attr("href",quickActions[1].actions[0].url);
+			$("nav .action-list:nth-child(2) li:nth-child(2) a" ).attr("href",quickActions[1].actions[1].url);
+			$("nav .action-list:nth-child(2) li:last-child a" ).attr("href",quickActions[1].actions[2].url);
+			$("nav .action-list:nth-child(2) li:first-child a" ).html(quickActions[1].actions[0].label);
+			$("nav .action-list:nth-child(2) li:nth-child(2) a" ).html(quickActions[1].actions[1].label);
+			$("nav .action-list:nth-child(2) li:last-child a" ).html(quickActions[1].actions[2].label);
 
-			$(".action-list:last-child li:first-child a").attr("href",quickActions[2].actions[0].url);
-			$(".action-list:last-child li:nth-child(2) a").attr("href",quickActions[2].actions[1].url);
-			$(".action-list:last-child li:last-child a").attr("href",quickActions[2].actions[2].url);
-			$(".action-list:last-child li:first-child a").html(quickActions[2].actions[0].label);
-			$(".action-list:last-child li:nth-child(2) a").html(quickActions[2].actions[1].label);
-			$(".action-list:last-child li:last-child a").html(quickActions[2].actions[2].label);
+			$("nav .action-list:last-child li:first-child a").attr("href",quickActions[2].actions[0].url);
+			$("nav .action-list:last-child li:nth-child(2) a").attr("href",quickActions[2].actions[1].url);
+			$("nav .action-list:last-child li:last-child a").attr("href",quickActions[2].actions[2].url);
+			$("nav .action-list:last-child li:first-child a").html(quickActions[2].actions[0].label);
+			$("nav .action-list:last-child li:nth-child(2) a").html(quickActions[2].actions[1].label);
+			$("nav .action-list:last-child li:last-child a").html(quickActions[2].actions[2].label);
 
 		});
 	}
