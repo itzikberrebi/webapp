dbc226835ec156f542bfe9259ba0a09a6d2d6fc0
itzikberrebi <itzikb800@gmail.com>
2015-08-24 22:36:08 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -3,199 +3,6 @@ jQuery(document).ready(function() {
 	var tabs = '';
 	var first = 1;
 
-	$.fn.loadJson = function(){ 
-		$.getJSON("data/config.json", function(data){
-			tabs = data.tabsList;
-			console.log('first json ' + tabs);
-			$('input[name="site1name"]').val(tabs[0].options.sites[0].name);
-			$('input[name="site1url"]').val(tabs[0].options.sites[0].url);
-			$('input[name="site2name"]').val(tabs[0].options.sites[1].name);
-			$('input[name="site2url"]').val(tabs[0].options.sites[1].url);
-			$('input[name="site3name"]').val(tabs[0].options.sites[2].name);
-			$('input[name="site3url"]').val(tabs[0].options.sites[2].url);
-			
-			$("#my-folders iframe").attr("src", tabs[1].options.url);
-
-			$("#public-folders iframe").attr("src", tabs[3].options.url);
-
-		});
-	}
-
-	$.fn.loadnotification = function(){ 
-		$.getJSON("data/config.json", function(data){
-			console.log('notification');
-			$( ".notifications" ).html( "<p>" + data.notification + "</p>" );
-		});
-	}
-
-	$.fn.loadquickActions = function(){ 
-		$.getJSON("data/config.json", function(data){
-			var quickActions=data.quickActions;
-			console.log(quickActions[1].actions[2].label);
-			$("nav #action-list1 li:first-child a" ).attr("href",quickActions[0].actions[0].url);
-			$("nav #action-list1 li:first-child a" ).html(quickActions[0].actions[0].label);
-			$("nav #action-list1 li:nth-child(2) a" ).attr("href",quickActions[0].actions[1].url);
-			$("nav #action-list1 li:nth-child(2) a" ).html(quickActions[0].actions[1].label);
-			$("nav #action-list1 li:last-child a" ).attr("href",quickActions[0].actions[2].url);
-			$("nav #action-list1 li:last-child a" ).html(quickActions[0].actions[2].label);
-
-			$("nav #action-list2 li:first-child a" ).attr("href",quickActions[1].actions[0].url);
-			$("nav #action-list2 li:nth-child(2) a" ).attr("href",quickActions[1].actions[1].url);
-			$("nav #action-list2 li:last-child a" ).attr("href",quickActions[1].actions[2].url);
-			$("nav #action-list2 li:last-child a" ).attr("href",quickActions[1].actions[3].url);
-			$("nav #action-list2 li:first-child a" ).html(quickActions[1].actions[0].label);
-			$("nav #action-list2 li:nth-child(2) a" ).html(quickActions[1].actions[1].label);
-			$("nav #action-list2 li:last-child a" ).html(quickActions[1].actions[2].label);
-			$("nav #action-list2 li:last-child a" ).html(quickActions[1].actions[3].label);
-
-			$("nav #action-list3 li:first-child a").attr("href",quickActions[2].actions[0].url);
-			$("nav #action-list3 li:nth-child(2) a").attr("href",quickActions[2].actions[1].url);
-			$("nav #action-list3 li:last-child a").attr("href",quickActions[2].actions[2].url);
-			$("nav #action-list3 li:first-child a").html(quickActions[2].actions[0].label);
-			$("nav #action-list3 li:nth-child(2) a").html(quickActions[2].actions[1].label);
-			$("nav #action-list3 li:last-child a").html(quickActions[2].actions[2].label);
-
-		});
-}
-
-$.fn.loadJsonSecondery = function(){
-	console.log('load for second time')
-	$('input[name="site1name"]').val(tabs[0].options.sites[0].name);
-	$('input[name="site1url"]').val(tabs[0].options.sites[0].url);
-	$('input[name="site2name"]').val(tabs[0].options.sites[1].name);
-	$('input[name="site2url"]').val(tabs[0].options.sites[1].url);
-	$('input[name="site3name"]').val(tabs[0].options.sites[2].name);
-	$('input[name="site3url"]').val(tabs[0].options.sites[2].url);
-}
-
-$.fn.saveJson = function(){ 
-	console.log('saving');
-	var tabsNew = {
-		"options": {
-			"rowLabel": "Report",
-			"sites": [
-			{"name":$('input[name="site1name"]').val(), "url": $('input[name="site1url"]').val()},
-			{"name":$('input[name="site2name"]').val(), "url":$('input[name="site2url"]').val()},
-			{"name":$('input[name="site3name"]').val(), "url":$('input[name="site3url"]').val()}
-			]
-		}
-	};
-	tabs[0]=tabsNew;
-}
-
-$.fn.closeSettings = function(){
-	jQuery('#sites-div').removeClass('sites-div-turnon').addClass('sites-div-turnoff');
-}
-
-$.fn.showFirstIframe = function() {
-	if ($('input[name="site1name"]').val()){
-		$("#quick-reports iframe").attr("src", tabs[0].options.sites[0].url);
-	} else if ($('input[name="site2name"]').val()) {
-		$("#quick-reports iframe").attr("src", tabs[0].options.sites[1].url);
-	} else if ($('input[name="site3name"]').val()) {
-		$("#quick-reports iframe").attr("src", tabs[0].options.sites[2].url);
-	}
-}
-
-$.fn.selectItemCheck = function() {
-	if ($('input[name="site1name"]').val() || $('input[name="site2name"]').val() || $('input[name="site3name"]').val()){
-		$('select').css("visibility", "visible");
-		$('select').empty();
-		for (var i = 1; i < 4; i++) {
-			if ($('input[name="site'+i+'name"]').val()) {
-				console.log($('input[name="site'+i+'name"]').val());
-				$('#quick-reports select').append('<option value="'+i+'">'+$('input[name="site'+i+'name"]').val()+'</option>');
-			};
-		};
-    		    // $('#quick-reports option[value="1"]').text($('input[name="site1name"]').val());
-    		    // $('#quick-reports option[value="2"]').text($('input[name="site2name"]').val());
-    		    // $('#quick-reports option[value="3"]').text($('input[name="site3name"]').val());
-
-    		} else {
-    			$('select').css("visibility", "hidden");			
-    		}
-    	}
-
-    	$.fn.addhttp = function() {
-		// $('input[name="site3url"]').val();
-		// $('input[name="site3url"]').val();
-		// $('input[name="site3url"]').val();
-	}
-
-	$.fn.isUrlValid = function(url) {
-		if (/^(http|https|ftp):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i.test(url)){
-			console.log("valid");
-			return 1;
-		} else {
-			console.log("not valid");
-			return 0;			
-		}
-	}
-
-	$.fn.checkUrl = function() {
-		var valid=1;
-
-		if ($('input[name="site1url"]').val() && $('input[name="site1name"]').val()) {
-			if ($.fn.isUrlValid($('input[name="site1url"]').val())){
-				$('#form-ul li:first-child input').css("background-color", "white");
-			} else {
-				$('#form-ul li:first-child input').css("background-color", "red");
-				valid=0;
-			}
-		} else if ($('input[name="site1url"]').val() || $('input[name="site1name"]').val()) {
-			$('#form-ul li:first-child input').css("background-color", "red");
-			valid=0;
-		}
-		
-		if ($('input[name="site2url"]').val() && $('input[name="site2name"]').val()) {
-			if ($.fn.isUrlValid($('input[name="site2url"]').val())){
-				$('#form-ul li:nth-child(2) input').css("background-color", "white");
-			} else {
-				$('#form-ul li:nth-child(2) input').css("background-color", "red");
-				valid=0;			
-			}
-		} else if ($('input[name="site2url"]').val() || $('input[name="site2name"]').val()) {
-			$('#form-ul li:nth-child(2) input').css("background-color", "red");
-			valid=0;
-		}
-		
-		if ($('input[name="site3url"]').val() && $('input[name="site3name"]').val()) {
-			if ($.fn.isUrlValid($('input[name="site3url"]').val())){
-				$('#form-ul li:last-child input').css("background-color", "white");
-			} else {
-				$('#form-ul li:last-child input').css("background-color", "red");
-				valid=0;
-			} 
-		}
-		else if ($('input[name="site3url"]').val() || $('input[name="site3name"]').val()) {
-			$('#form-ul li:last-child input').css("background-color", "red");
-			valid=0;
-		}
-		return valid;
-	}
-
-	$('#quick-reports select').change(function() {
-		var eventTypeName = $("#quick-reports option:selected");
-		if (eventTypeName.is('[value="1"]') ) {
-			$("#quick-reports iframe").attr("src", tabs[0].options.sites[0].url);
-		}
-		if (eventTypeName.is('[value="2"]') ) {
-			$("#quick-reports iframe").attr("src", tabs[0].options.sites[1].url);
-		}
-		if (eventTypeName.is('[value="3"]') ) {
-			$("#quick-reports iframe").attr("src", tabs[0].options.sites[2].url);
-		}
-	});	
-
-	jQuery('.tabs li a').on('click', function(e){
-		var href = jQuery(this).attr('href');
-		jQuery(href).show().removeClass('tab_turnoff').addClass('tab_turnon');
-		jQuery(href).siblings('div').hide().removeClass('tab_turnon').addClass('tab_turnoff');
-		jQuery(this).parent('li').removeClass('turnoff').addClass('turnon');
-		jQuery(this).parent('li').siblings().removeClass('turnon').addClass('turnoff');
-		e.preventDefault();
-	});
-
 	$.fn.bringDivName = function(this_item){
 		var temp = jQuery(this_item).parents();
 		console.log(temp);
@@ -209,12 +16,159 @@ $.fn.selectItemCheck = function() {
 		};
 	}
 
+	$.fn.loadJson = function(){ 
+		$.getJSON("data/config.json", function(data){
+			tabs = data.tabsList;
+			for (var i = 0; i < 5; i++) {
+				$('input[name="site'+(i+1)+'name"]').val(tabs[0].options.sites[i].name);
+				$('input[name="site'+(i+1)+'url"]').val(tabs[0].options.sites[i].url);
+			};			
+			$("#my-folders iframe").attr("src", tabs[1].options.url);
+			$("#public-folders iframe").attr("src", tabs[3].options.url);
+		});
+	}
+
+	$.fn.loadnotification = function(){ 
+		$.getJSON("data/config.json", function(data){
+			console.log('notification');
+			$( ".notifications" ).html( "<p>" + data.notification + "</p>" );
+		});
+	}
+
+	$.fn.loadquickActions = function(){ 
+		$.getJSON("data/config.json", function(data){
+			var array=["first-child","nth-child(2)","last-child"];
+			var array=["first-child","nth-child(2)","nth-child(3)","last-child"];
+			var quickActions=data.quickActions;
+			var jloop;
+			for (var i = 0; i < 3; i++) {
+				if (i==1) {jloop=4} else {jloop=3}
+					for (var j = 0; j < jloop; j++) {
+						$("nav #action-list"+i+" li:"+array[j]+" a").attr("href",quickActions[i].actions[j].url);
+						$("nav #action-list"+i+" li:"+array[j]+" a").html(quickActions[i].actions[j].label);
+					};
+				};
+			});
+	}
+
+	$.fn.loadJsonSecondery = function(){
+		for (var i = 0; i < 6; i++) {
+			$('input[name="site'+(i+1)+'name"]').val(tabs[0].options.sites[i].name);
+			$('input[name="site'+(i+1)+'url"]').val(tabs[0].options.sites[i].url);
+		};
+	}
+
+	$.fn.saveJson = function(){ 
+		console.log('saving');
+		var tabsNew = {
+			"options": {
+				"rowLabel": "Report",
+				"sites": [
+				{"name":$('input[name="site1name"]').val(), "url": $('input[name="site1url"]').val()},
+				{"name":$('input[name="site2name"]').val(), "url":$('input[name="site2url"]').val()},
+				{"name":$('input[name="site3name"]').val(), "url":$('input[name="site3url"]').val()}
+				{"name":$('input[name="site4name"]').val(), "url":$('input[name="site4url"]').val()}
+				{"name":$('input[name="site5name"]').val(), "url":$('input[name="site5url"]').val()}
+				{"name":$('input[name="site6name"]').val(), "url":$('input[name="site6url"]').val()}
+				]
+			}
+		};
+		tabs[0]=tabsNew;
+	}
+
+	$.fn.closeSettings = function(div_name){
+		jQuery('#' +div_name+ ' #sites-div').removeClass('sites-div-turnon').addClass('sites-div-turnoff');
+	}
+
+	$.fn.showFirstIframe = function(div_name) {
+
+		for (var i = 0; i < 6; i++) {
+			if ($('input[name="site'+(i+1)+'name"]').val()){
+				$("#"+div_name+" iframe").attr("src", tabs[0].options.sites[i].url);
+			} 
+		};
+	}
+
+	$.fn.selectItemCheck = function(div_name) {
+		var j;
+		if (div_name == 'quick-reports') {
+			j=0;
+		} else {
+			j=3;
+		}
+		if ($('input[name="site'+j+'name"]').val() || $('input[name="site'+(j+1)+'name"]').val() || $('input[name="site'+(j+2)+'name"]').val()){
+			$('select').css("visibility", "visible");
+			$('select').empty();
+
+			for (var i = (1+j); i < (4+j); i++) {
+				if ($('input[name="site'+i+'name"]').val()) {
+					$('#'+div_name+' select').append('<option value="'+i+'">'+$('input[name="site'+i+'name"]').val()+'</option>');
+				};
+			};
+		} else {
+			$('select').css("visibility", "hidden");			
+		}
+	}
+
+	$.fn.addhttp = function() {
+	}
+
+	$.fn.isUrlValid = function(url) {
+		if (/^(http|https|ftp):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i.test(url)){
+			console.log("valid");
+			return 1;
+		} else {
+			console.log("not valid");
+			return 0;			
+		}
+	}
+
+	$.fn.checkUrl = function(div_name) {
+		var valid=1;
+		for (var i = i; i < 7; i++) {
+			if ($('input[name="site'+i+'url"]').val() && $('input[name="site'+i+'name"]').val()) {
+				if ($.fn.isUrlValid($('input[name="site'+i+'url"]').val())){
+					$('#'+div_name+'#form-ul li:first-child input').css("background-color", "white");
+				} else {
+					$('#'+div_name+'#form-ul li:first-child input').css("background-color", "red");
+					valid=0;
+				}
+			} else if ($('input[name="site'+i+'url"]').val() || $('input[name="site'+i+'name"]').val()) {
+				$('#'+div_name+'#form-ul li:first-child input').css("background-color", "red");
+				valid=0;
+			}
+		};
+		return valid;
+	}
+
+	$('select').change(function() {
+		var div_name = $.fn.bringDivName(this);
+		var eventTypeName = $("#"+div_name+" option:selected");
+		if (div_name == 'quick-reports') {
+			j=1;
+		} else {
+			j=4;
+		}
+		for (var i = j; i < (j+3); i++) {
+			if (eventTypeName.is('[value="'+i+'"]') ) {
+				$("#"+div_name+" iframe").attr("src", tabs[0].options.sites[(i-1)].url);
+			}
+		};
+	});	
+
+	jQuery('.tabs li a').on('click', function(e){
+		var href = jQuery(this).attr('href');
+		jQuery(href).show().removeClass('tab_turnoff').addClass('tab_turnon');
+		jQuery(href).siblings('div').hide().removeClass('tab_turnon').addClass('tab_turnoff');
+		jQuery(this).parent('li').removeClass('turnoff').addClass('turnon');
+		jQuery(this).parent('li').siblings().removeClass('turnon').addClass('turnoff');
+		e.preventDefault();
+	});
+
 	jQuery('.settings').on('click', function(e)  {
 		var div_name = $.fn.bringDivName(this);
-		console.log (div_name);
-		$.fn.loadJsonSecondery();
+		$.fn.loadJsonSecondery(div_name);
 		console.log('after pressing settings for second time');			
-		console.log(tabs);
 		e.preventDefault();
 		var class_name = jQuery('#'+div_name+' #sites-div').attr('class');
 		if (class_name=='sites-div-turnoff') {
@@ -226,8 +180,9 @@ $.fn.selectItemCheck = function() {
 		};
 	});
 
-	jQuery('#expand').on('click', function(e)  {
-		var class_name = jQuery('iframe').attr('src');
+	jQuery('.expand').on('click', function(e)  {
+		var div_name = $.fn.bringDivName(this);
+		var class_name = jQuery('#'+div_name+' iframe').attr('src');
 		if (class_name!=null) {
 			var win = window.open(class_name);
 			win.focus();
@@ -235,21 +190,23 @@ $.fn.selectItemCheck = function() {
 		}
 	});
 
-	jQuery('#cancel').on('click', function(e)  {
-		jQuery('#sites-div').removeClass('sites-div-turnon').addClass('sites-div-turnoff');
-		$.fn.closeSettings();
+	jQuery('.cancel').on('click', function(e)  {
+		var div_name = $.fn.bringDivName(this);
+		jQuery('#'+div_name+' #sites-div').removeClass('sites-div-turnon').addClass('sites-div-turnoff');
+		$.fn.closeSettings(div_name);
 		e.preventDefault();
 	});
 
-	jQuery("#quick-reports form").submit(function(e){
+	jQuery("form").submit(function(e){
+		var div_name = $.fn.bringDivName(this);
 		$.fn.addhttp();
 		if ($.fn.checkUrl()){
 			$.fn.saveJson();
 			console.log('submit');
 			console.log(tabs);
-			$.fn.closeSettings();
-			$.fn.selectItemCheck();
-			$.fn.showFirstIframe();
+			$.fn.closeSettings(div_name);
+			$.fn.selectItemCheck(div_name);
+			$.fn.showFirstIframe(div_name);
 		} else {
 			console.log("submit fail");
 		}
@@ -257,23 +214,29 @@ $.fn.selectItemCheck = function() {
 	});
 
 	$.fn.removeAttrSelected = function() {
-		$('#quick-reports option[value="1"]').removeAttr("selected");		
-		$('#quick-reports option[value="2"]').removeAttr("selected");		
-		$('#quick-reports option[value="3"]').removeAttr("selected");		
+		for (var i = 1; i < 7; i++) {
+			$('option[value="'+i+'"]').removeAttr("selected");		
+		};
 	}
 
 	jQuery(".search-box").submit(function(e){
+		var div_name = $.fn.bringDivName(this);
 		console.log('search submit');
-		console.log($('input[name="q"]').val());
 		var found=0;
-		for (var i = 0; i < 3; i++) {
+		for (var i = 0; i < 6; i++) {
 			if (tabs[0].options.sites[i].name.toLowerCase() == $('input[name="q"]').val().toLowerCase()) {
 				found=1;
-				$.fn.closeSettings();
-				$.fn.removeAttrSelected();
-				$( ".notifications" ).html( "<p>" + "</p>" );			
-				$('#quick-reports option[value="'+i+'"]').attr("selected","selected");		
-				$('#quick-reports select').change();
+				$.fn.closeSettings(div_name);
+				$.fn.removeAttrSelected(div_name);
+				$( ".notifications" ).html( "<p>" + "</p>" );
+				
+				if (i<3) {			
+					$('#quick-reports option[value="'+i+'"]').attr("selected","selected");		
+					$('#quick-reports select').change();
+				} else {
+					$('#my-team-folders option[value="'+i+'"]').attr("selected","selected");		
+					$('#my-team-folders select').change();
+				}
 			}
 		};
 		if (!found) {
